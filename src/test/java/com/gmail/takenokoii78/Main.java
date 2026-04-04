ecddcca38ffbf35a0ae00becd84d847f087769d3
topaz <takenokoii78@gmail.com>
2026-04-05 01:01:50 +0900

    java25

--- src/test/java/com/gmail/takenokoii78/Main.java
+++ src/test/java/com/gmail/takenokoii78/Main.java
@@ -1,7 +1,24 @@
 package com.gmail.takenokoii78;
 
+import com.gmail.takenokoii78.mojangson.MojangsonParser;
+import com.gmail.takenokoii78.mojangson.MojangsonPath;
+
 public class Main {
-    public static void main(String[] args) {
+    static void main() {
         System.out.println("Hello world!");
+
+        final var c = MojangsonParser.compound(
+            """
+            {
+                foo: bar,
+                ints: [I; 0, 1, 2, 3],
+                comp: {
+                    key: 0xffffff
+                }
+            }
+            """
+        );
+
+        System.out.println(c.getTypeOf(MojangsonPath.of("comp.key")));
     }
 }
