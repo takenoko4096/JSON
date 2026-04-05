b8db169d36189467e9cbabb0079cb27c48f2df2e
たけのこII <yu2.aoki0229.@gmail.com>
2026-04-05 15:56:37 +0900

    a

--- src/test/java/com/gmail/takenokoii78/Main.java
+++ src/test/java/com/gmail/takenokoii78/Main.java
@@ -1,24 +1,3 @@
-package com.gmail.takenokoii78;
-
-import com.gmail.takenokoii78.mojangson.MojangsonParser;
-import com.gmail.takenokoii78.mojangson.MojangsonPath;
-
-public class Main {
-    static void main() {
-        System.out.println("Hello world!");
-
-        final var c = MojangsonParser.compound(
-            """
-            {
-                foo: bar,
-                ints: [I; 0, 1, 2, 3],
-                comp: {
-                    key: 0xffffff
-                }
-            }
-            """
-        );
-
-        System.out.println(c.getTypeOf(MojangsonPath.of("comp.key")));
-    }
+void main() {
+    IO.println("Hello, World!");
 }
